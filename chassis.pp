f2bc1e3bc294216ca3ef63d012f6a391a82e84da
Matthew Haines-Young <matthew@matth.eu>
2017-03-07 13:41:17 +0000

    Local tester config

--- chassis.pp
+++ chassis.pp
@@ -1,2 +1,11 @@
+$tester_config = sz_load_config()
+
 class {"tester": }
-class {"tester::config": }
+class {"tester::config":
+	wpdir              => $tester_config[mapped_paths][wp],
+	contentdir         => $tester_config[mapped_paths][content],
+	tester_db_name     => $tester_config[tester_db][name],
+	tester_db_user     => $tester_config[tester_db][user],
+	tester_db_password => $tester_config[tester_db][password],
+	tester_db_prefix   => $tester_config[tester_db][prefix]
+}
--- modules/tester/manifests/config.pp
+++ modules/tester/manifests/config.pp
@@ -1,6 +1,17 @@
-class tester::config {
+class tester::config(
+	$tester_db_name = 'tests',
+	$tester_db_user = 'tester',
+	$tester_db_password = 'vagrant_password',
+	$tester_db_prefix = 'tests',
+	$wpdir = 'wp',
+	$contentdir = 'content'
+) {
 	file { "/vagrant/extensions/tester/wpdevel/wp-tests-config.php":
 		content => template("tester/wp-tests-config.php.erb"),
+	}
+
+	file { '/vagrant/local-config-tester.php':
+		content => template('tester/local-config-tester.php.erb')
 	}
 
 	file { "/etc/profile.d/tester-env.sh":
